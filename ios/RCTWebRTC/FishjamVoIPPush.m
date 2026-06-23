872a22870c3e6d64e6495e2bee01003ccbba9876
Paweł Gadomski <xpfk.098@gmail.com>
2026-06-23 09:48:53 +0200

    Change queue to a serial one

--- ios/RCTWebRTC/FishjamVoIPPush.m
+++ ios/RCTWebRTC/FishjamVoIPPush.m
@@ -4,7 +4,8 @@
 
 @interface FishjamVoIPPush () <PKPushRegistryDelegate>
 @property(nonatomic, strong) PKPushRegistry *registry;
-@property(nonatomic, copy, readwrite, nullable) NSString *token;
+@property(nonatomic, strong) dispatch_queue_t registryQueue;
+@property(copy, readwrite, nullable) NSString *token;
 @end
 
 @implementation FishjamVoIPPush
@@ -20,7 +21,6 @@
 }
 
 + (void)registerForVoIPPushes {
-    NSLog(@"[FishjamVoIPPush] Inicjalizacja PKPushRegistry dla VoIP...");
     [[self shared] registerForVoIPPushes];
 }
 
@@ -29,8 +29,8 @@
         return;
     }
     
-    // TODO: apple recommends serial queue for this param, will have to investigate
-    self.registry = [[PKPushRegistry alloc] initWithQueue:dispatch_get_main_queue()];
+    self.registryQueue = dispatch_queue_create("io.fishjam.voippush", DISPATCH_QUEUE_SERIAL);
+    self.registry = [[PKPushRegistry alloc] initWithQueue:self.registryQueue];
     self.registry.delegate = self;
     self.registry.desiredPushTypes = [NSSet setWithObject:PKPushTypeVoIP];
 }
@@ -38,16 +38,13 @@
 #pragma mark - PKPushRegistryDelegate
 
 - (void)pushRegistry:(PKPushRegistry *)registry didUpdatePushCredentials:(PKPushCredentials *)pushCredentials forType:(PKPushType)type {
-    NSLog(@"[FishjamVoIPPush] Otrzymano/Zaktualizowano VoIP Push Credentials.");
-    
     const unsigned char *bytes = pushCredentials.token.bytes;
     NSMutableString *hex = [NSMutableString stringWithCapacity:pushCredentials.token.length * 2];
     for (NSUInteger i = 0; i < pushCredentials.token.length; i++) {
         [hex appendFormat:@"%02x", bytes[i]];
     }
     NSString *tokenString = [hex copy];
-    
-    NSLog(@"[FishjamVoIPPush] Wygenerowany Token: %@", tokenString);
+
     if ([tokenString isEqualToString:self.token]) {
         return;
     }
@@ -64,7 +61,6 @@
 
 - (void)pushRegistry:(PKPushRegistry *)registry didReceiveIncomingPushWithPayload:(PKPushPayload *)payload forType:(PKPushType)type withCompletionHandler:(void (^)(void))completion {
     NSDictionary *dict = payload.dictionaryPayload;
-    NSLog(@"[FishjamVoIPPush] OTRZYMANO PUSH! Pełny payload: %@", dict);
     NSString *displayName = dict[@"displayName"] ?: dict[@"username"];
     BOOL isVideo = [dict[@"isVideo"] boolValue];
     
