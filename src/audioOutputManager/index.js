8c27c983fb720143b26aa6e375cbd0e2891121ca
Paweł Gadomski <xpfk.098@gmail.com>
2026-07-28 16:32:40 +0200

    Fix

--- src/audioOutputManager/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AudioDeviceType = exports.AudioOutputManager = void 0;
-var AudioOutputManager_1 = require("./AudioOutputManager");
-Object.defineProperty(exports, "AudioOutputManager", { enumerable: true, get: function () { return AudioOutputManager_1.AudioOutputManager; } });
-Object.defineProperty(exports, "AudioDeviceType", { enumerable: true, get: function () { return AudioOutputManager_1.AudioDeviceType; } });
